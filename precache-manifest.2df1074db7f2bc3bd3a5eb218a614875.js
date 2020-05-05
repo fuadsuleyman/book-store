9085a2459c009d38952719f22e3357e71e774f4f
fuad_suleymanov <fuadsuleymanov@outlook.com>
2020-05-05 12:05:31 +0400

    Updates

---
+++ precache-manifest.2df1074db7f2bc3bd3a5eb218a614875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d948dc1585ed05f661fe0b32e1ca6a",
+    "url": "/fuadsuleyman/book-store/index.html"
+  },
+  {
+    "revision": "0feec54c6d8c9586d900",
+    "url": "/fuadsuleyman/book-store/static/css/main.51f91c59.chunk.css"
+  },
+  {
+    "revision": "bf9e9a0eb7d9cea77c7d",
+    "url": "/fuadsuleyman/book-store/static/js/2.551995e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fuadsuleyman/book-store/static/js/2.551995e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0feec54c6d8c9586d900",
+    "url": "/fuadsuleyman/book-store/static/js/main.11472686.chunk.js"
+  },
+  {
+    "revision": "45a3118f8ff2553a59b9",
+    "url": "/fuadsuleyman/book-store/static/js/runtime-main.dca962a2.js"
+  },
+  {
+    "revision": "955a1270d121e0f7b828a66079ed9c07",
+    "url": "/fuadsuleyman/book-store/static/media/error.955a1270.png"
+  }
+]);
